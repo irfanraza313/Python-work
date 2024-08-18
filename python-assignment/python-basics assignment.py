34df38553ea615c5dfc370e33fef46789aac4c13
Irfan Raza <irfanraza22@gmail.com>
2024-08-19 00:10:20 +0500

    list-assiggnment.ipynb

---
+++ python-assignment/python-basics assignment.py
@@ -0,0 +1,67 @@
+message='salam to everyone'                # Task 1
+print(message)
+message= 'respect yourself' # first value  # Task 2
+print(message)
+message= 'respect others' # change value
+print(message)
+name= "Raza"                               # Task 3
+print (f'"Salam {name}, would you like to learn some python today?"')
+name= "imran"                              # Task 4
+print(name.lower())
+print(name.upper())
+print(name.title())
+author= "Imam Ali as"                      # Task 5
+quote= "knowledge is power, but it is useless without action."
+print (f'{author} once said, "{quote}"')
+famous_person= "Imam Mahdi as,"            # Task 6
+message= "the promised saviour."
+print(famous_person, message)
+name= "\n   Irfan Raza   \t"               # Task 7
+name_stripped= name.strip()
+print(name_stripped)
+name_lstripped=name.lstrip()
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name_lstripped)
+name_rstripped= name.rstrip()
+print(name_rstripped)
+x=5                                       # Task 8
+y=10
+z=15
+print("the sum is:",x+y+z)
+a=5                                       # Task 9
+b=12
+a,b=b,a # swapping the values
+print("a=",a)
+print("b=",b)
+favorite_color= "blue"                    # Task 10
+print(favorite_color)
+best_color= "blue"
+print(best_color)
+pet_name="Buddy"                          # Task 11
+print(pet_name)
+pet_name='Max'
+print(pet_name)
+score = 100                               # Task 13
+print(score)
+score=70
+print(score)
+city = "Karbala"                          # Task 14
+print(city)
+text = "python programming"               # Task 15
+print(text.title())
+mixed_cases= 'KAracHi'                     # Task 16              
+print(mixed_cases.lower())
+mixed_cases= 'PAkistan'                    # Task 17
+print(mixed_cases.upper())
+temperature=25                              # Task 18
+print(f'"The current temperature is {temperature} degrees."')
+poem= "\nFather strong like a tree,\nHeart warm like the sun,\nLove deep like the sea." # Task 19
+print(poem)
+name='sunshine'                           # Task 12
+print(name)
+print(sunsine)
